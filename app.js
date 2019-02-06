7a1ce3447d99a7f327e2bc387deebc1d7a78af54
Steve Clements <steve@originaleye.co.uk>
2019-02-06 15:49:21 +0000

    wip: template splitting

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var mainRouter = require('./routes/main');
+var chapter1 = require('./routes/chapter1');
 var nunjucks = require('nunjucks')
 
 
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/main', mainRouter);
+app.use('/chapter1', chapter1);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/chapter1.js
+++ routes/chapter1.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('chapter1');
+  res.render('chapter1/index.html');
 });
 
 
