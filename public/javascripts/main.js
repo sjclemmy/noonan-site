f804b9c837b5c0f5fe54744332ba43825f7487d4
Steve Clements <steve@originaleye.co.uk>
2019-02-11 15:41:21 +0000

    new lightbox with zoom

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,66 @@
-function openViewer() {
-  const viewer = new Viewer(document.getElementById(this.id));
+let currentHeight = 0;
+let zoomFactor = 0;
+
+function viewImage(imageName) {
+
+  const instance = basicLightbox.create(`
+    <div class="image-modal-container">
+    <div class="button-container">
+        <button class="zoom-button" onclick="zoom('in')">in</button>
+        <button class="zoom-button" onclick="zoom('out')">out</button>  
+        <button class="zoom-button" onclick="download()">download</button>  
+    </div>
+        <div class="image-container">
+            <img id="displayed-image" src="${imageName}" />
+        </div>
+    </div>`
+  ).show();
+
 
 }
+
+function viewTable(id) {
+
+  const table = document.querySelector('#'+id).cloneNode(true)
+
+
+
+
+  const el =  `
+    <div class="image-modal-container">
+    <div class="button-container">
+        <button class="zoom-button" onclick="zoom('in')">in</button>
+        <button class="zoom-button" onclick="zoom('out')">out</button>
+    </div>
+        <div class="image-container">
+        ${table.innerHTML}
+        </div>
+    </div>`;
+
+  console.log('t', el);
+
+  const instance = basicLightbox.create(el).show();
+}
+
+
+function zoom(inOrOut) {
+  const image = document.getElementById('displayed-image');
+  if (currentHeight === 0) {
+    currentHeight = image.offsetHeight;
+    zoomFactor = currentHeight / 2;
+  }
+  if (inOrOut === 'in') {
+    currentHeight = currentHeight + zoomFactor;
+  } else {
+    currentHeight = currentHeight - zoomFactor;
+  }
+
+  image.style.height = currentHeight + 'px';
+}
+
+
+
+
+
 
 
