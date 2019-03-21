88de6ba9d0985499110083ac1a3bf42c27fc74cd
Chipembene <47394385+Chipembene@users.noreply.github.com>
2019-03-21 21:51:15 +0000

    Nearly complete content to end Ch7

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -112,13 +112,45 @@
 
 // Chapter 3 accordion
 
-var acc = document.getElementsByClassName("poorcard-header");
+var pooracc = document.getElementsByClassName("poorcard-header");
+var pooracc1 = document.getElementsByClassName("poornoshow");
 var i;
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() 
+for (i = 0; i < pooracc.length; i++) {
+           pooracc[i].addEventListener("click", function() 
+	             {closeall(i);
+	              this.classList.toggle("active1");
+	              var panel = this.nextElementSibling;
+                  if (panel.style.display === "block")
+				  {panel.style.display = "none";}
+			      else
+				  {panel.style.display = "block";}
+			     }
+				                      );
+				                     }
+	
+
+
+function closeall(z) {
+var p=0;
+
+do	{
+pooracc[p].classList.remove("active1");
+pooracc1[p].style.display = "none";
+p++;
+}
+while (p < pooracc1.length, p != z);
+}
+	
+// Chapter 5 accordion
+
+var clareacc = document.getElementsByClassName("clarecard-header");
+var j;
+
+for (j = 0; j < clareacc.length; j++) {
+    clareacc[j].addEventListener("click", function() 
 	{
-	this.classList.toggle("active1");
+	this.classList.toggle("active2");
 	var panel = this.nextElementSibling;
     if (panel.style.display === "block") 
 	{panel.style.display = "none";}
@@ -127,7 +159,5 @@ for (i = 0; i < acc.length; i++) {
     }
 	);
 }
-
-
 
 
